1b16884bd4ed4353ea6b66a69320ef88b68476cd
RobLui <robbertluit@hotmail.com>
2017-01-27 01:16:48 +0100

    adding bg & scrolling into game

--- first_game/main.js
+++ first_game/main.js
@@ -1,2 +1,26 @@
 //GAME
-var game = "";
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
+
+var spacefield;
+var background_x;
+
+//MAIN STATE (array of functions)
+var mainState = {
+    preload: function() {
+        // Preload the image so we can use it in the game, ID en LOCATIE gebruikt
+        game.load.image('_mountains', 'assets/_mountains.jpg');
+    },
+    create: function() {
+        //Add the image to the screen , COORDINATEN en ID gebruikt
+        spacefield = game.add.tileSprite(0, 0, 800, 600, '_mountains');
+        background_x = 4;
+    },
+    //update will happen every frame -> bv een bewegend karakter
+    update: function() {
+        //update the current y position of the image every frame
+        spacefield.tilePosition.x += background_x;
+
+    }
+}
+game.state.add('mainState', mainState);
+game.state.start('mainState');
