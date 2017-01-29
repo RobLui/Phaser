1b5331ac0fafe24eb846441c7fbfc7aff3abeaca
RobLui <robbertluit@hotmail.com>
2017-01-29 15:19:39 +0100

    remove yt game tutorial basics, not working, add new phaser.io guide game tryout

--- first_game/main.js
+++ first_game/main.js
@@ -1,46 +1,66 @@
 //GAME
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
 
+// 800 & 600 determine how large the canvas size will be
+// Phaser.auto determines the render mode -- GameDiv is the dom element where the game gets placed in
+// The array at last are the essential functions that are used in the game
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'GameDiv', {
+    preload: preload,
+    create: create,
+    update: update
+});
 var spacefield;
 var background_x;
-var player;
 var cursors;
+var player;
 
 //MAIN STATE (array of functions)
-var mainState = {
-    preload: function() {
-        // Preload the image so we can use it in the game, ID en LOCATIE gebruikt
-        game.load.image('_mountains', 'assets/_mountains.jpg');
-        game.load.image('_player', 'assets/_player.png');
-    },
-    create: function() {
-        //  -- BACKGROUND --
-        //Add the image to the screen , COORDINATEN en ID gebruikt
-        spacefield = game.add.tileSprite(0, 0, 800, 600, '_mountains');
-        //move the background every frame
-        background_x = 4;
+function preload() {
 
-        // -- PLAYER --
-        //Add the player to specific location bij het begin van het spel
-        player = spacefield.add.sprite(game.world.centerX, game.world.centerY + 200, '_player');
-        //Add psychics to the player
-        game.physics.enable(player, Phaser.Physics.ARCADE);
-        //creer the link between the arrows
-        cursors = game.input.keyboard.CreateCursorKeys();
-    },
-    //update will happen every frame -> bv een bewegend karakter
-    update: function() {
-        //update the current y position of the image every frame
-        spacefield.tilePosition.x += background_x;
-        player.velocity.x = 0;
-        //check op keys pressed
-        if (cursors.left.isDown) {
-            player.velocity.x = -350;
-        }
-        if (cursors.right.isDown) {
-            player.velocity.x = 350;
-        }
-    }
-};
-game.state.add('mainState', mainState);
-game.state.start('mainState');
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
+}
+
+function create() {
+
+    var platforms;
+
+    // Enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Background for the game
+    game.add.sprite(0, 0, 'sky');
+
+    // The platforms group contains the ground and the two ledges we can jump on
+    platforms = game.add.group();
+
+    // Enable physics for any object that is created in this group
+    platforms.enableBody = true;
+
+    // Create the ground
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+
+    // Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
+
+    // This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    // Create two ledges
+    var ledge = platforms.create(400, 400, 'ground');
+
+    // This also stops it from falling away when you jump on it
+    ledge.body.immovable = true;
+
+    // Put it on a certain position
+    ledge = platforms.create(-150, 250, 'ground');
+
+    // This also stops it from falling away when you jump on it
+    ledge.body.immovable = true;
+}
+//update will happen every frame -> bv een bewegend karakter
+function update() {
+
+}
